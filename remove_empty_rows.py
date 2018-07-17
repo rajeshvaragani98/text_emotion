c245d929bfdecf48b8e312ff77164bb04f95674b
Rajesh Varagani <rajeshvaragani12345@gmail.com>
2018-07-17 10:25:14 +0530

    Update remove_empty_rows.py

--- remove_empty_rows.py
+++ remove_empty_rows.py
@@ -1,3 +1,12 @@
+import csv
 import pandas as pd
-df = pd.read_csv('trail.csv')
-df.to_csv('trail_removed.csv', index=False)
+l = [330,335,346,347,348,350,351,352,353,355,356,362,367,377,381,418,440,483]
+l2= [330,335]
+for k in l:
+    temp =[]
+    result_file = csv.reader(open(str(str(k)+"_"+"output.csv") , "r"))
+    result = open(str(str(k)+"output_final.csv") , "w")
+    wr = csv.writer(result)
+    for i in result_file:
+        if(i):
+            wr.writerow(i)
